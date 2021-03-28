3cf3500291be56a9d2f9994f5e1a469399695339
jiamen <2731275645@qq.com>
2021-03-28 10:31:47 +0800

    lidar_localization

---
+++ cmake/PCL.cmake
@@ -0,0 +1,7 @@
+
+find_package(PCL 1.7 REQUIRED)
+list(REMOVE_ITEM PCL_LIBRARIES "vtkproj4")
+
+include_directories(${PCL_INCLUDE_DIRS})
+list(APPEND ALL_TYPE_LIBRARIES ${PCL_LIBRARIES})
+
---
+++ src/apps/data_pretreat_node.cpp
@@ -0,0 +1,12 @@
+//
+// Created by zlc on 2021/3/28.
+//
+
+#include <ros/ros.h>
+#include <glog/logging.h>
+
+#include "lidar_localization/global_defination/global_defination.h"
+#include "lidar_localization/data_pretreat/data_pretreat_flow.hpp"
+
+
+
